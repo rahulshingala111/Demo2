41359660d2107f2adb775f06e355f2f3577b8fee
Rahul Shingala <rahulshingala111@gmail.com>
2022-01-01 15:42:24 +0530

    Revert "5th loop"
    This reverts commit 3d4d82e37fda03738dd3bc926b6552285acb87fb.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,7 @@ public class Main {
 
     for(int i=1;i<=5;i++)
     {
-        for(int j=1;j<i;j++) {
-            System.out.println("No ." + i + "" + j);
-        }
+        System.out.println("Nodfhgdfzhffzdhfdzhdfzhdh ."+i);
     }
     }
 }
